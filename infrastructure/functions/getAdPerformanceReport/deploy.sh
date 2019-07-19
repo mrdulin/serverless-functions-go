6395ea6495fb55654d3cb89b4625414011274fca
mrdulin <novaline.dulin@gmail.com>
2019-07-19 19:17:41 +0800

    set env vars for cloud function

--- infrastructure/functions/getAdPerformanceReport/deploy.sh
+++ infrastructure/functions/getAdPerformanceReport/deploy.sh
@@ -2,5 +2,4 @@
 
 cd "$(dirname "$0")"
 export GO111MODULE=on
-go mod vendor && gcloud functions deploy GetAdPerformanceReport --runtime=go111 --trigger-topic get-ad-performance-report
-#--env-vars-file .env.yaml
+go mod vendor && gcloud functions deploy GetAdPerformanceReport --runtime=go111 --trigger-topic get-ad-performance-report --env-vars ENV=production
